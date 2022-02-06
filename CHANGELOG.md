bfecf934c6f5800a3f5dcf3c802732fb874153b0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-02-06 12:37:35 +0100

    added a changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+# Changelog
+
+## Summary
+
+- [Changelog](#changelog)
+  - [Summary](#summary)
+  - [0.1.0](#010)
+    - [Major changes](#major-changes)
+    - [Minor changes](#minor-changes)
+    - [Bug & security fixes](#bug--security-fixes)
+
+## 0.1.0
+
+[Summary](#summary)
+
+### Major changes
+
+None.
+
+[Summary](#summary)
+
+### Minor changes
+
+None.
+
+[Summary](#summary)
+
+### Bug & security fixes
+
+None.
+
+[Summary](#summary)
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,10 +1,11 @@
 import {resolve} from "path";
-import esbuild from "rollup-plugin-esbuild";
 import {external} from "@aminnairi/rollup-plugin-external";
+import {terser} from "rollup-plugin-terser";
+import esbuild from "rollup-plugin-esbuild";
 
 export default {
   input: resolve("index.ts"),
-  plugins: [esbuild(), external()],
+  plugins: [external(), esbuild(), terser()],
   output: {
     file: resolve("build", "index.js"),
     format: "cjs"
